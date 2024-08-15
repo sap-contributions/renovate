acd3cabf0563ddce564a418a0cdb61b831290d8f
Nicolas Bender <nicolas.bender@sap.com>
2024-08-15 08:28:55 +0200

    Actually use the provided snapshot

--- lib/modules/manager/piper-config/extract.spec.ts
+++ lib/modules/manager/piper-config/extract.spec.ts
@@ -29,50 +29,7 @@ describe('modules/manager/piper-config/extract', () => {
         packageFile,
         config,
       );
-      expect(result).toMatchSnapshot({
-        deps: [
-        {
-            "autoReplaceStringTemplate": "{{depName}}{{#if newValue}}:{{newValue}}{{/if}}{{#if newDigest}}@{{newDigest}}{{/if}}",
-           "currentDigest": undefined,
-           "currentValue": "3.9.6",
-           "datasource": "docker",
-           "depName": "maven",
-           "replaceString": "maven:3.9.6",
-         },
-        {
-           "autoReplaceStringTemplate": "{{depName}}{{#if newValue}}:{{newValue}}{{/if}}{{#if newDigest}}@{{newDigest}}{{/if}}",
-           "currentDigest": undefined,
-           "currentValue": "8-jdk17-alpine",
-           "datasource": "docker",
-           "depName": "gradle",
-           "replaceString": "gradle:8-jdk17-alpine",
-         },
-          {
-           "autoReplaceStringTemplate": "{{depName}}{{#if newValue}}:{{newValue}}{{/if}}{{#if newDigest}}@{{newDigest}}{{/if}}",
-           "currentDigest": undefined,
-           "currentValue": "11",
-           "datasource": "docker",
-           "depName": "openjdk",
-           "replaceString": "openjdk:11",
-         },
-          {
-           "autoReplaceStringTemplate": "{{depName}}{{#if newValue}}:{{newValue}}{{/if}}{{#if newDigest}}@{{newDigest}}{{/if}}",
-           "currentDigest": undefined,
-           "currentValue": "0.0.2",
-           "datasource": "docker",
-            "depName": "docker.io/paketobuildpacks/build-noble-base",
-           "replaceString": "docker.io/paketobuildpacks/build-noble-base:0.0.2",
-          },
-          {
-           "autoReplaceStringTemplate": "{{depName}}{{#if newValue}}:{{newValue}}{{/if}}{{#if newDigest}}@{{newDigest}}{{/if}}",
-           "currentDigest": undefined,
-           "currentValue": "0.0.2",
-           "datasource": "docker",
-            "depName": "index.docker.io/paketobuildpacks/run-noble-base",
-           "replaceString": "index.docker.io/paketobuildpacks/run-noble-base:0.0.2",
-          },
-        ],
-      });
+      expect(result).toMatchSnapshot();
     });
   });
 });
